0735e6f884a6676906c89b6160a72060e4970f0a
vladimka <zverevv.04@gmail.com>
2025-02-03 02:01:25 +0800

    images

--- index.js
+++ index.js
@@ -19,27 +19,67 @@ bot.command('clear', async ctx => {
 });
 
 bot.on(message('text'), async ctx => {
-    console.log(ctx.from.id, ctx.message.text);
-    let msg = await ctx.reply('Генерирую ответ...');
-    let modelContext;
+    try{
+        let msg = await ctx.reply('Генерирую ответ...');
+        let modelContext;
 
-    if((modelContext = modelContexts.find(el => el.userId == ctx.from.id)) == undefined){
-        modelContext = {
-            userId : ctx.from.id,
-            messages : []
-        };
-        modelContexts.push(modelContext);
+        if((modelContext = modelContexts.find(el => el.userId == ctx.from.id)) == undefined){
+            modelContext = {
+                userId : ctx.from.id,
+                messages : []
+            };
+            modelContexts.push(modelContext);
+        }
+
+        modelContext.messages.push({ role : 'user', content : ctx.message.text });
+        const response = await ollama.chat({
+            model,
+            messages : modelContext.messages
+        });
+
+        modelContext.messages.push({ role : 'assistant', content : response.message.content });
+        console.log(response.message.content);
+        await bot.telegram.editMessageText(msg.chat.id, msg.message_id, undefined, response.message.content);
+    }catch(e){
+        console.error(e);
+        await ctx.reply('Произошла ошибка. Попробуйте снова.');
     }
+});
 
-    modelContext.messages.push({ role : 'user', content : ctx.message.text });
-    const response = await ollama.chat({
-        model,
-        messages : modelContext.messages
-    });
-
-    modelContext.messages.push({ role : 'assistant', content : response.message.content });
-    console.log(modelContext);
-    await bot.telegram.editMessageText(msg.chat.id, msg.message_id, undefined, response.message.content);
+bot.on(message('photo'), async ctx => {
+    try{
+        const { file_id } = ctx.message.photo.pop();
+        let photo;
+    
+        const link = await bot.telegram.getFileLink(file_id);
+        await fetch(link.toString())
+            .then(res => res.bytes())
+            .then(res => photo = res);
+    
+        let msg = await ctx.reply('Генерирую ответ...');
+        let modelContext;
+    
+        if((modelContext = modelContexts.find(el => el.userId == ctx.from.id)) == undefined){
+            modelContext = {
+                userId : ctx.from.id,
+                messages : []
+            };
+            modelContexts.push(modelContext);
+        }
+    
+        modelContext.messages.push({ role : 'user', content : ctx.message.caption, images : [photo] });
+        const response = await ollama.chat({
+            model,
+            messages : modelContext.messages
+        });
+    
+        modelContext.messages.push({ role : 'assistant', content : response.message.content });
+        console.log(response.message.content);
+        await bot.telegram.editMessageText(msg.chat.id, msg.message_id, undefined, response.message.content);
+    }catch(e){
+        console.error(e);
+        await ctx.reply('Произошла ошибка. Попробуйте снова.');
+    }
 });
 
 bot.launch();
